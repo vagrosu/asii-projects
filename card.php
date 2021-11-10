a0b9bafcbcce8f1eca906f7c0edc5fa2d5288915
Petrenkko <petrisor_vmy@yahoo.com>
2021-11-11 00:17:34 +0200

    Creare sablon + inserare carduri + lista_proiecte

---
+++ card.php
@@ -0,0 +1,10 @@
+<div class="col-md-4 cols">
+                <div class="card">
+                    <img class="card-img-top" src="..." alt="Card image cap">
+                    <div class="card-body">
+                      <h5 class="card-title"> <?php echo $proiecte[$i]["nume"] ?> </h5>
+                      <p class="card-text"> <?php echo $proiecte[$i]["descriere"] ?> </p>
+                      <a href="<?php echo $proiecte[$i]["site"] ?>" class="btn btn-primary">Viziteaza site</a>
+                    </div>
+                  </div>
+            </div>
