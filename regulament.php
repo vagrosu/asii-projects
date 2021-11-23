10cbb9d288e8e4c9644480eca0782e545bf44d43
lGnyte <grasu.ioan02@gmail.com>
2021-11-23 22:15:01 +0200

    file management update

--- regulament.php
+++ regulament.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="icon" href="logo-icon.png">
+    <link rel="icon" href="img/logo-icon.png">
      <!-- BOOTSTRAP INCLUDE -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="./media_regulament/regulament.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/regulament.css">
     <title>Regulament - LAN Party</title>
 </head>
 <body>
@@ -210,6 +210,6 @@
     <?php
         include("footer.php");
     ?>
-    <script src="carousel-script.js"></script>
+    <script src="assets/js/carousel-script.js"></script>
 </body>
 </html>
