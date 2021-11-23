e2cc8bb42a358c03de31d46fb9145ec76326abce
Victor511 <vagrosu@yahoo.com>
2021-11-23 23:23:32 +0200

    Footer restyle

--- footer.php
+++ footer.php
@@ -1,33 +1,44 @@
 <?php echo '
 <div class="" id="footer">
     <div class="container">
-        <div class="row">
-            <div class="col col-lg-2" id="logo">
+        <div class="row mt-5">
+            <div class="col" id="logo">
                 <img src="img/logo.png">
             </div>
-            <div class="col col-lg-6 text-start" id="coordonatori">
+            <div class="col text-start" id="coordonatori">
                 Coordonator: Teodor Dragoi
                 <br>
                 Email: teodorfdragoi@gmail.com
             </div>
-            <div class="col col-lg-4 text-center" id="fb-event">
+            <div class="col text-end" id="fb-event">
                 <a class="btn btn-outline-info" href="https://www.facebook.com/events/833132907028241/">
-                    Lan Party
+                    EVENIMENT
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                    </svg>
+                </a>
+            </div>
+            <div class="col text-end" id="asii-logo">
+                <a href="https://www.asii.ro/">
+                    <img src="img/asii.png" />
                 </a>
             </div>
         </div>
-        <div class="" id="asii-logo">
-            <img src="img/asii.png" />
+        <div class="row mt-5">
+            <div class="text-center" id="despre">
+                Asociaţia Studenţilor Informaticieni Ieşeni (ASII) este reprezentata de un grup de 
+                persoane determinate, creative si dinamice, ce au in comun pasiunea pentru Informatica 
+                si impartasesc un set de teluri si valori. Activitatile organizate in cadrul asociatiei 
+                se adreseaza studentilor si profesorilor Facultatii de Informatica Iasi, dar si comunitatii 
+                IT din Romania, dorind sa promoveze proactivitatea, dezvoltarea personala si sociala.
+            </div>
         </div>
-        <div class="text-center" id="despre">
-            Asociaţia Studenţilor Informaticieni Ieşeni (ASII) este reprezentata de un grup de 
-            persoane determinate, creative si dinamice, ce au in comun pasiunea pentru Informatica 
-            si impartasesc un set de teluri si valori. Activitatile organizate in cadrul asociatiei 
-            se adreseaza studentilor si profesorilor Facultatii de Informatica Iasi, dar si comunitatii 
-            IT din Romania, dorind sa promoveze proactivitatea, dezvoltarea personala si sociala.
-        </div>
-        <div class="text-center" id="copyright">
-            ASII © 2021
+        <div class="row mt-5">
+            <div class="col align-slef-end">
+                <div class="text-center" id="copyright">
+                   ASII © 2021
+                </div>
+            </div>
         </div>
     </div>
 </div>
