6f018c78923664280a5e76df26b7df0d09722d15
lGnyte <grasu.ioan02@gmail.com>
2021-11-25 00:28:28 +0200

    Advanced header - not responsive

---
+++ navbar.php
@@ -0,0 +1,14 @@
+<?php
+echo '<div id="navbar">
+  <a href="index.php"><img src="img/logo.png" id="logo"></a>
+  <div class="title">
+    <span>LAN Party 2019 - 16/17 Martie</span>
+  </div>
+  <div class="navbar-buttons right">
+    <a href="regulament.php">Regulament</a>
+    <a href="contact.php">Contact</a>
+    <a href="#inscriere">Înscrie-te</a>
+  </div>
+</div>'
+
+ ?>
